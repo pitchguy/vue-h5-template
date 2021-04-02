0b7707085b5d03cc1c149201c11901940eecc2c0
chen jie <jie.chen@shui-inc.com>
2021-04-02 17:21:00 +0800

    添加storage过期方法

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 export const router = [
   {
     path: '/',
@@ -9,17 +9,27 @@ export const router = [
     component: () => import('@/views/home/index'), // 路由懒加载
     meta: {
       title: '首页', // 页面标题
-      keepAlive: false // keep-alive 标识
-    }
-  }
-]
+      keepAlive: false, // keep-alive 标识
+    },
+  },
+  {
+    path: '/404',
+    name: 'error404',
+    component: () => import('@/views/error/404'),
+    meta: { title: '404页面', keepAlive: false },
+  },
+  {
+    path: '*',
+    redirect: '/404',
+  },
+];
 
 const createRouter = () =>
   new Router({
     // mode: 'history', // 如果你是 history模式 需要配置vue.config.js publicPath
     // base: '/app/',
-    scrollBehavior: () => ({y: 0}),
-    routes: router
-  })
+    scrollBehavior: () => ({ y: 0 }),
+    routes: router,
+  });
 
-export default createRouter()
+export default createRouter();
--- src/utils/index.js
+++ src/utils/index.js
@@ -10,20 +10,20 @@
  */
 export function parseTime(time, cFormat) {
   if (arguments.length === 0) {
-    return null
+    return null;
   }
-  const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
-  let date
+  const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}';
+  let date;
   if (typeof time === 'object') {
-    date = time
+    date = time;
   } else {
-    if ((typeof time === 'string') && (/^[0-9]+$/.test(time))) {
-      time = parseInt(time)
+    if (typeof time === 'string' && /^[0-9]+$/.test(time)) {
+      time = parseInt(time);
     }
-    if ((typeof time === 'number') && (time.toString().length === 10)) {
-      time = time * 1000
+    if (typeof time === 'number' && time.toString().length === 10) {
+      time = time * 1000;
     }
-    date = new Date(time)
+    date = new Date(time);
   }
   const formatObj = {
     y: date.getFullYear(),
@@ -32,18 +32,20 @@ export function parseTime(time, cFormat) {
     h: date.getHours(),
     i: date.getMinutes(),
     s: date.getSeconds(),
-    a: date.getDay()
-  }
+    a: date.getDay(),
+  };
   const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
-    let value = formatObj[key]
+    let value = formatObj[key];
     // Note: getDay() returns 0 on Sunday
-    if (key === 'a') { return ['日', '一', '二', '三', '四', '五', '六'][value ] }
-    if (result.length > 0 && value < 10) {
-      value = '0' + value
+    if (key === 'a') {
+      return ['日', '一', '二', '三', '四', '五', '六'][value];
     }
-    return value || 0
-  })
-  return time_str
+    if (result.length > 0 && value < 10) {
+      value = '0' + value;
+    }
+    return value || 0;
+  });
+  return time_str;
 }
 
 /**
@@ -53,39 +55,29 @@ export function parseTime(time, cFormat) {
  */
 export function formatTime(time, option) {
   if (('' + time).length === 10) {
-    time = parseInt(time) * 1000
+    time = parseInt(time) * 1000;
   } else {
-    time = +time
+    time = +time;
   }
-  const d = new Date(time)
-  const now = Date.now()
+  const d = new Date(time);
+  const now = Date.now();
 
-  const diff = (now - d) / 1000
+  const diff = (now - d) / 1000;
 
   if (diff < 30) {
-    return '刚刚'
+    return '刚刚';
   } else if (diff < 3600) {
     // less 1 hour
-    return Math.ceil(diff / 60) + '分钟前'
+    return Math.ceil(diff / 60) + '分钟前';
   } else if (diff < 3600 * 24) {
-    return Math.ceil(diff / 3600) + '小时前'
+    return Math.ceil(diff / 3600) + '小时前';
   } else if (diff < 3600 * 24 * 2) {
-    return '1天前'
+    return '1天前';
   }
   if (option) {
-    return parseTime(time, option)
+    return parseTime(time, option);
   } else {
-    return (
-      d.getMonth() +
-      1 +
-      '月' +
-      d.getDate() +
-      '日' +
-      d.getHours() +
-      '时' +
-      d.getMinutes() +
-      '分'
-    )
+    return d.getMonth() + 1 + '月' + d.getDate() + '日' + d.getHours() + '时' + d.getMinutes() + '分';
   }
 }
 
@@ -94,9 +86,9 @@ export function formatTime(time, option) {
  * @returns {Object}
  */
 export function param2Obj(url) {
-  const search = url.split('?')[1]
+  const search = url.split('?')[1];
   if (!search) {
-    return {}
+    return {};
   }
   return JSON.parse(
     '{"' +
@@ -106,5 +98,62 @@ export function param2Obj(url) {
         .replace(/=/g, '":"')
         .replace(/\+/g, ' ') +
       '"}'
-  )
+  );
 }
+
+/**
+ * localStorage
+ * @调用：_local.set('access_token', '123456', 5000);
+ * @调用：_local.get('access_token');
+ */
+export const _local = {
+  //存储,可设置过期时间
+  set(key, value, expires) {
+    let params = { key, value, expires };
+    if (expires) {
+      // 记录何时将值存入缓存，毫秒级
+      var data = Object.assign(params, { startTime: new Date().getTime() });
+      localStorage.setItem(key, JSON.stringify(data));
+    } else {
+      if (Object.prototype.toString.call(value) == '[object Object]') {
+        value = JSON.stringify(value);
+      }
+      if (Object.prototype.toString.call(value) == '[object Array]') {
+        value = JSON.stringify(value);
+      }
+      localStorage.setItem(key, value);
+    }
+  },
+  //取出
+  get(key) {
+    let item = localStorage.getItem(key);
+    // 先将拿到的试着进行json转为对象的形式
+    try {
+      item = JSON.parse(item);
+    } catch (error) {
+      // eslint-disable-next-line no-self-assign
+      item = item;
+    }
+    // 如果有startTime的值，说明设置了失效时间
+    if (item && item.startTime) {
+      let date = new Date().getTime();
+      // 如果大于就是过期了，如果小于或等于就还没过期
+      if (date - item.startTime > item.expires) {
+        localStorage.removeItem(name);
+        return false;
+      } else {
+        return item.value;
+      }
+    } else {
+      return item;
+    }
+  },
+  // 删除
+  remove(key) {
+    localStorage.removeItem(key);
+  },
+  // 清除全部
+  clear() {
+    localStorage.clear();
+  },
+};
