6193b9c192ac73b17fcd4d6d2027b366257c43e5
chen jie <jie.chen@shui-inc.com>
2021-04-02 17:28:22 +0800

    按需引入

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,7 +1,39 @@
 // 按需全局引入 vant组件
-import Vue from 'vue'
-import {Button, List, Cell, Tabbar, TabbarItem} from 'vant'
-Vue.use(Button)
-Vue.use(Cell)
-Vue.use(List)
-Vue.use(Tabbar).use(TabbarItem)
+// 自行引入
+import Vue from 'vue';
+import {
+  Empty,
+  Button,
+  List,
+  Cell,
+  Field,
+  Tabbar,
+  TabbarItem,
+  Form,
+  Tab,
+  Tabs,
+  Toast,
+  Popup,
+  ActionSheet,
+  Checkbox,
+  CheckboxGroup,
+  Popover,
+  Icon,
+} from 'vant';
+
+Vue.use(Empty);
+Vue.use(Button);
+Vue.use(Cell);
+Vue.use(List);
+Vue.use(Form);
+Vue.use(Tab);
+Vue.use(Tabs);
+Vue.use(Field);
+Vue.use(Toast);
+Vue.use(Popup);
+Vue.use(ActionSheet);
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
+Vue.use(Popover);
+Vue.use(Icon);
+Vue.use(Tabbar).use(TabbarItem);
