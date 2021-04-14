e07d3101b986ab6f1ac96516063e406d9afab6b9
chen jie <jie.chen@shui-inc.com>
2021-04-14 20:06:37 +0800

    test页面添加动效 --update

---
+++ src/views/testPage/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div className="company-fl">
+    <h2>7000+合作伙伴信赖</h2>
+    <h4>专注小微经济体业·财·税服务</h4>
+    <div className="move-contain">
+      <div className="move-mask move-mask-left"></div>
+      <div className="move-mask move-mask-right"></div>
+      <div className="line-move">
+        <Image alt="" src="/images/home-logo1.png" width="1710" height="109" />
+        <Image alt="" src="/images/home-logo1.png" width="1710" height="109" />
+      </div>
+      <div className="line-move line-even">
+        <Image alt="" src="/images/home-logo2.png" width="1710" height="109" />
+        <Image alt="" src="/images/home-logo2.png" width="1710" height="109" />
+      </div>
+      <div className="line-move">
+        <Image alt="" src="/images/home-logo3.png" width="1710" height="109" />
+        <Image alt="" src="/images/home-logo3.png" width="1710" height="109" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
